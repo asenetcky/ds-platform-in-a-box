850f5c5b9cacfec2f8d015a6184855c9b089bc6b
asenetcky <asenetcky@gmail.com>
2025-02-28 20:59:24 -0500

    feat: explict localhost machine

--- start-rstudio.sh
+++ start-rstudio.sh
@@ -4,7 +4,8 @@ docker run \
   --env-file .env \
   --rm \
   -ti \
-  -p 8787:8787 \
-  -v /data:/data \
+  -p 127.0.0.1:8787:8787 \
+  -v /data:/root/data \
   -v ${PWD}/.config/:/root/.config/ \
-  rstudio:0.0.0.9999
+  -v ${PWD}/.Rprofile/:/root/.Rprofile \
+  asenetcky/ds-rstudio:0.1.0
