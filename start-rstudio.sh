c13a8925f9171446a6ce63baff752649335452d5
asenetcky <asenetcky@gmail.com>
2025-03-09 09:44:43 -0400

    fix: fix env parsing

--- start-rstudio.sh
+++ start-rstudio.sh
@@ -3,11 +3,14 @@
 bash create-env.sh
 
 docker run \
+  --name ds-rstudio \
   --env-file .env \
   --rm \
   -tid \
   -p 127.0.0.1:8787:8787 \
   -v ${HOME}/data:/root/data \
-  \
-  asenetcky/ds-rstudio # -v ${PWD}/.config/:/root/.config/ \
-# -v ${PWD}/.Rprofile/:/root/.Rprofile \
+  asenetcky/ds-rstudio
+# -v ${PWD}/.Rprofile/:/root/.Rprofile
+# # -v ${PWD}/.config/:/root/.config/ \
+
+rm -rf .env
