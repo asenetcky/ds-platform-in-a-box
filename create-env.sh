ea69e5feca89d12529ae6754217b5c4321ea1204
asenetcky <asenetcky@gmail.com>
2025-03-09 09:37:31 -0400

    fix: remove double quotes for env parsing

--- create-env.sh
+++ create-env.sh
@@ -3,5 +3,3 @@
 # Use 1password cli to create a .env file from a template
 # and inject secrets into it
 op inject -i env.tmpl -o .env
-
-#TODO setup a 1password service account
--- start-rstudio.sh
+++ start-rstudio.sh
@@ -3,7 +3,7 @@
 bash create-env.sh
 
 docker run \
-  --env-file .env \
+  --env-file ${PWD}/.env \
   --rm \
   -tid \
   -p 127.0.0.1:8787:8787 \
@@ -11,3 +11,5 @@ docker run \
   -v ${PWD}/.config/:/root/.config/ \
   -v ${PWD}/.Rprofile/:/root/.Rprofile \
   asenetcky/ds-rstudio
+
+rm -rf .env
